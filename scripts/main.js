7d5590d34aa7ff4ec95b212ce56da0298ba01f36
Shane Bell <being.for.itself@gmail.com>
2020-12-10 20:22:21 -0500

    Working on BSP generation.
    Oh god the recursion.  This may wait for more coffee.

--- scripts/main.js
+++ scripts/main.js
@@ -3,35 +3,88 @@ function getRandomInt(max) {
 }
 
 class Level {
-    constructor(w, h, t_w, t_h) {
-        this.height = Math.floor(h/t_h);
-        this.width = Math.floor(w/t_w);
-        this.tile_width = t_w;
-        this.tile_height = t_h;
-        this.cells = new Array(this.width * this.height);
-        this.walls = [];
-        this.tiles = new Array(this.width * this.height);
+    constructor(level) {
+        this.pixels = {
+            height: level.height,
+            width: level.width,
+            grid: level.grid,
+            padding: level.padding
+        };
+
+        this.tiles = {
+            height: Math.floor(this.pixels.grid/this.pixels.height),
+            width: Math.floor(this.pixels.grid/this.pixels.width)
+        };
+
+        this.map = {
+            cells: new Array(this.tiles.height * this.tiles.width),
+            walls: [],
+            tiles: new Array(this.tiles.height * this.tiles.width)
+        };
     }
 
-    generate() {
-        // Just places random tiles for now, as a test.
-        for (let y = 0; y < this.height; y++) {
-            for (let x = 0; x < this.width; x++) {
-                let cell_type = Math.floor(Math.random() * Math.floor(3));
-                this.cells[x+y*this.width] = cell_type;
-                this.tiles[x+y*this.width] = Tile.create({
-                    img: "./modules/random_dungeon/assets/" + cell_type + ".png",
-                    width: this.tile_width,
-                    height: this.tile_height,
-                    scale: 1,
-                    x: x*this.tile_width,
-                    y: y*this.tile_width,
-                    rotation: 0,
-                    hidden: false,
-                    locked: false
-                });
-            }
+    generateLevelBSP(depth=1, grid={ l:0, r:this.tiles.width, t:0, b:this.tiles.height, subgrids:[]}) {
+        if (depth < 0) {
+            return;
         }
+        if (depth%2) {
+            grid.subgrids.push({
+                l: 0,
+                r: Math.floor(grid.r/2),
+                t: 0,
+                b: Math.floor(grid.b/2),
+                subgrids:[]
+            })
+            grid.subgrids.push({
+                l: Math.floor(grid.r/2),
+                r: grid.r,
+                t: Math.floor(grid.b/2),
+                b: grid.b,
+                subgrids:[]
+            })
+        } else {
+            grid.subgrids.push({
+                l: 0,
+                r: Math.floor(grid.r/2),
+                t: 0,
+                b: Math.floor(grid.b/2),
+                subgrids:[]
+            })
+            grid.subgrids.push({
+                l: Math.floor(grid.r/2),
+                r: grid.r,
+                t: Math.floor(grid.b/2),
+                b: grid.b,
+                subgrids:[]
+            })
+        }
+        
+        grid.subgrids.forEach(sub => {
+            this.generateLevelBSP(depth--, sub);
+        });
+
+        return grid;
+    }
+
+    generateLayout() {
+        // Just places random tiles for now, as a test.
+        // for (let y = 0; y < this.height; y++) {
+        //     for (let x = 0; x < this.width; x++) {
+        //         let cell_type = Math.floor(Math.random() * Math.floor(3));
+        //         this.cells[x+y*this.width] = cell_type;
+        //         this.tiles[x+y*this.width] = Tile.create({
+        //             img: "./modules/random_dungeon/assets/" + cell_type + ".png",
+        //             width: this.tile_width,
+        //             height: this.tile_height,
+        //             scale: 1,
+        //             x: x*this.tile_width,
+        //             y: y*this.tile_width,
+        //             rotation: 0,
+        //             hidden: false,
+        //             locked: false
+        //         });
+        //     }
+        // }
     }
 
     render(scene) {
@@ -41,8 +94,7 @@ class Level {
 
 Hooks.on("ready", _ => {
     console.log("RNG Dungeons: Ding.");
-    scene = game.scenes.active.data;
-    l = new Level(scene.width, scene.height, scene.grid, scene.grid);
-    l.generate();   
-    l.render(scene);
+    l = new Level(game.scenes.active.data);
+    // l.generate();   
+    // l.render(game.scenes.active.data);
 });
